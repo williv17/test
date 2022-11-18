7b2773ab9a9cf1326c341b2c34b1c0c8d5336081
williv17 <willi.vargas17@gmail.com>
2022-11-18 15:30:43 -0800

    restructure

---
+++ 667teamProject/backend/db/server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const pgp = require('pg-promise')();
+
+console.log(process.env.DATABASE_URL);
+
+const connection = pgp(process.env.DATABASE_URL);
+
+module.exports = connection;
