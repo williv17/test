1283fdf80b82bff3e08fcb05933ef6d1af96b5f0
williv17 <willi.vargas17@gmail.com>
2022-12-05 23:12:01 -0800

    Co-authored-by: EJ Gonzalez <ejg132@gmail.com>

--- 667teamProject/backend/routes/register.js
+++ 667teamProject/backend/routes/register.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var registerRouter = express.Router();
+const registerController = require('../controllers/registerController.js');
 
 registerRouter.get('/', function (request, response, next) {
   response.render('public_views/register', { error: false });
 });
+
+registerRouter.post('/', registerController.registerUser);
+
 
 module.exports = registerRouter;
--- 667teamProject/backend/routes/scripts.js
+++
@@ -1,8 +0,0 @@
-// const express = require('express');
-// const scriptRouter = express.Router();
-// const path = require('path');
-// const handleRegister = require('../../frontend/js/registerHandler.js');
-
-// scriptRouter.get('/registerHandler.js', function (request, response, next) {
-
-// module.exports = scriptRouter;
