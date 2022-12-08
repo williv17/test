10f275b1f459edd6ac198fa4516441ca3c8ae2d4
williv17 <willi.vargas17@gmail.com>
2022-12-08 01:20:06 -0800

    added dynamic nav to rules rout

--- 667teamProject/backend/routes/rules.js
+++ 667teamProject/backend/routes/rules.js
@@ -2,7 +2,12 @@ var express = require('express');
 var rulesRouter = express.Router();
 
 rulesRouter.get('/', function(request, response, next) {
-  response.render('public_views/rules');
+  let access_token;
+  if (request.cookies) {
+    access_token = request.cookies['jwt'];
+  }
+
+  response.render('public_views/rules', { access_token: access_token });
 });
 
 module.exports = rulesRouter;
